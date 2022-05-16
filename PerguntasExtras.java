9861acb0508b943f7a0cbc54c2620f8b9c79523c
luanmdc <101462627+luanmdc@users.noreply.github.com>
2022-05-15 21:58:27 -0300

    Primeiro Git commit

---
+++ PerguntasExtras.java
@@ -0,0 +1,25 @@
+public class PerguntasExtras {
+}
+/*Perguntas
+        1 - O que é um objeto/classe?
+        Representação de itens fisicos, ideias ou conceitos abstratos do mundo real em um paradigma orientado a objetos.
+        Bloco de contrucao fundamental em sistemas OO, com responsabilidades bem definidas dentro da apliscacao.
+        Tipo personalizado de dados, "molde" para a criacao de objetos com propriedades e funcoes em comum.
+        Obejeto é representacao de um elemento do mundo real em um programa de computador
+
+        2 - O que é um atributo/variável?
+        Atributos sao as propriedades que uma classe tem.
+
+        3 - O que é git?
+        Git é um sistema de controle com a funcao de registrar e gravar quaisquer alteracoes feitas em cima de um codigo,
+        armazedando essas informacoes e permitindo que, um programador possa regregir ou adicionar versoes a uma aplicacao.
+
+        4 - O que é github?
+        E uma plataforma online onde voce pode criar repositorios e hospedar seus projetos, colaborar com sistemas open source,
+        O github armazena todos estes dados em nuvem e voce pode acessa-los de onde estiver.
+
+        5 - O que é um método?
+        Sao as funcionalidades da classe.
+
+
+ */
